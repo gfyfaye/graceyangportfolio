e898740ff81cca9d90667e56b20288efe6ad131a
gfyfaye <gogograce999@gmail.com>
2025-01-04 19:40:27 -0800

    Revamp portfolio homescreen

---
+++ js/test.js
@@ -0,0 +1,47 @@
+
+//stars twinkling
+
+var stars = document.querySelectorAll('.star');
+
+function twinkle(star) {
+    // random opacity to twinkle
+    star.style.opacity = Math.random() * 0.5 + 0.5;
+    // random timeout btw opacity changes
+    setTimeout(() => twinkle(star), Math.random() * 600 + 100);
+}
+
+function startTwinkling() {
+    stars.forEach((star) => {
+        twinkle(star); // Start individual twinkling
+    });
+}
+
+startTwinkling();
+
+
+
+
+//typewriter animation
+
+var messageArray = ["Grace Yang"];
+var nextMessage = ["Software Engineer"]
+var textPosition = 0;
+var speed = 150;
+const typewriterElement = document.querySelector("#typewriter");
+const fadeElement = document.querySelector("#fadeElement");
+
+typewriter = () => {
+    typewriterElement.innerHTML = messageArray[0].substring(0, textPosition) + "<span>\u25ae</span>";
+
+    if (textPosition++ != messageArray[0].length) {
+        setTimeout(typewriter, speed);}
+    else {
+        setTimeout(fadeInNextMessage, 500);
+    }
+}
+function fadeInNextMessage() {
+    fadeElement.innerHTML = nextMessage[0]; // Set the next message
+    fadeElement.style.opacity = 1; // Trigger the CSS transition
+}
+window.addEventListener("load", typewriter);
+
