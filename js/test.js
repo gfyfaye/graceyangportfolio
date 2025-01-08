c822289d5a12d7e4af7e29f8f9fa1011aa55c57d
gfyfaye <gogograce999@gmail.com>
2025-01-07 19:40:44 -0600

    skillsandresumesections

--- js/test.js
+++ js/test.js
@@ -29,6 +29,8 @@ var textPosition = 0;
 var speed = 150;
 const typewriterElement = document.querySelector("#typewriter");
 const fadeElement = document.querySelector("#fadeElement");
+const fadeButton1 = document.getElementById("GithubButton");
+const fadeButton2 = document.getElementById("LinkedInButton");
 
 typewriter = () => {
     typewriterElement.innerHTML = messageArray[0].substring(0, textPosition) + "<span class='blinking'>\u25ae</span>";
@@ -36,12 +38,23 @@ typewriter = () => {
     if (textPosition++ != messageArray[0].length) {
         setTimeout(typewriter, speed);}
     else {
-        setTimeout(fadeInNextMessage, 500);
+        setTimeout(fadeInNextItems, 500);
     }
 }
-function fadeInNextMessage() {
+function fadeInNextItems() {
     fadeElement.innerHTML = nextMessage[0]; // Set the next message
     fadeElement.style.opacity = 1; // Trigger the CSS transition
+
+    fadeButton1.style.opacity = 1;
+    fadeButton2.style.opacity = 1;
+
+    fadeButton1.style.pointerEvents = "auto";
+    fadeButton2.style.pointerEvents = "auto";
 }
 window.addEventListener("load", typewriter);
 
+
+const resumebutton = document.querySelector("#resume-button")
+function openResume() {
+    window.open('resume.pdf', '_blank');
+  }
